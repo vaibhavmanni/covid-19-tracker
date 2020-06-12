10807ea53c8238e27757f4d4919d81de88125a99
Vaibhav Manni <mannivaibhav95@gmail.com>
2020-06-12 20:40:07 +0530

    Updates

---
+++ precache-manifest.42a4d51076d9beb5e84ebe196e798c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cfafda31e357086639f990b6bed8bc",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "33eee21d185e04412e8f",
+    "url": "/covid-19-tracker/static/css/main.0e74641e.chunk.css"
+  },
+  {
+    "revision": "e1c648f3d765909f95bc",
+    "url": "/covid-19-tracker/static/js/2.c886ca40.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.c886ca40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33eee21d185e04412e8f",
+    "url": "/covid-19-tracker/static/js/main.e36d7bea.chunk.js"
+  },
+  {
+    "revision": "9a0aa31bd5430d866a30",
+    "url": "/covid-19-tracker/static/js/runtime-main.f8fbc3e1.js"
+  },
+  {
+    "revision": "4a894caf1e2c59a0d34881450c9a0868",
+    "url": "/covid-19-tracker/static/media/covid.4a894caf.png"
+  }
+]);
